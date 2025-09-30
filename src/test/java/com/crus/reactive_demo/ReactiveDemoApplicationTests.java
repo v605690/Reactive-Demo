dcd8681d854f1f2a0464c1ce5a9a484f857a75b2
v605690 <v605690@gmail.com>
2025-09-30 10:34:14 -0500

    Add reactive demo project

---
+++ src/test/java/com/crus/reactive_demo/ReactiveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crus.reactive_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
